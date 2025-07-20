afe46f0f81ed4953e0dac12a21f45296cd5bcf13
BoxThatBeat <buitenwerfa@hotmail.ca>
2025-07-20 12:00:23 -0400

    added initial logging

---
+++ Source/GptPawnDialogue/Mod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Verse;
+
+namespace GptPawnDialogue
+{
+    public sealed class Mod : Verse.Mod
+    {
+        public const string Id = "Bubbles";
+        public const string Name = "Interaction Bubbles";
+        public const string Version = "4.1";
+
+        public static Mod Instance = null;
+
+        public Mod(ModContentPack content) : base(content)
+        {
+            Instance = this;
+
+            Log("Initialized");
+        }
+
+        public static void Log(string message) => Verse.Log.Message(PrefixMessage(message));
+        public static void Warning(string message) => Verse.Log.Warning(PrefixMessage(message));
+        public static void Error(string message) => Verse.Log.Error(PrefixMessage(message));
+        private static string PrefixMessage(string message) => $"[{Name} v{Version}] {message}";
+    }
+}
